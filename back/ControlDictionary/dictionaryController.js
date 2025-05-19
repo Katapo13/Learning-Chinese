ebaf84d6dd3ace8274872fb4a52e19286da43081
Katapo13 <145489642+Katapo13@users.noreply.github.com>
2025-05-20 00:27:53 +0300

    back dictionary

---
+++ back/ControlDictionary/dictionaryController.js
@@ -0,0 +1,14 @@
+const Word = require('../models/Word');
+
+class dictionaryController{
+
+    async getWords(req, res){
+        try{
+            const words = await Word.find();
+            res.json(words);
+        }catch(e){
+            console.log(e);
+            res.status(500).json({ message: 'Server error' });
+        }
+    }
+}
--- back/models/Word.js
+++ back/models/Word.js
@@ -1,11 +1,11 @@
 const {Schema, model} = require ('mongoose')
 
 const Word = new Schema({
-    chinese: {type: String, unique: true, required: true},
+    chinese: {type: String, required: true},
     pinyin: {type: String, required: true},
     translation: {type: String, required: true},
-    partOfSpeech: {type: String, ref: 'PartOfSpeech'},
-    topic: {type: String, ref: 'Topic'}
+    partOfSpeech: {type: String, required: true},
+    topic: {type: String, required: true}
 })
 
 module.exports = model ('Word', Word)
